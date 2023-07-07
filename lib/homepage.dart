d90f9f8be121b2f3b58a75fcdde94d9549e6a49a
elsangra <koweros100@gmail.com>
2023-07-07 16:15:45 +0300

    homepage nav bar

---
+++ lib/homepage.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class navbar extends StatefulWidget {
+  const navbar({super.key});
+
+  @override
+  State<navbar> createState() => _navbarState();
+}
+
+class _navbarState extends State<navbar> {
+  @override
+  Widget build(BuildContext context) {
+    Size size=MediaQuery.of(context).size;
+    return Scaffold(
+      bottomNavigationBar: Container(
+        child: NavigationBar(
+            destinations: [
+              NavigationDestination(
+                  icon: Icon(Icons.home),
+                  label: "Home"
+              ),
+              NavigationDestination(
+                  icon: Icon(Icons.search),
+                  label: "Search"
+              ),
+              NavigationDestination(
+    icon: FloatingActionButton(
+    child: Icon(Icons.shopping_cart),
+    onPressed: (){},
+    ),
+    label: "Cart"
+                 ),
+              NavigationDestination(
+                  icon: Icon(Icons.favorite),
+                  label: "Wishlist"
+              ),
+              NavigationDestination(
+                  icon: Icon(Icons.person_outline_outlined),
+                  label: "Profile"
+              ),
+            ]
+        ),
+      ),
+
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/person2.jpg'),
-              ),
-              SizedBox(height: 10.0),
-              Text(
-                "Jim Jong",
-                style: TextStyle(
-                  fontFamily: "Pacifico",
-                  fontSize: 40.0,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                "MOBILE DEVELOPER",
-                style: TextStyle(
-                  fontFamily: "SourceSans",
-                  color: Colors.teal.shade100,
-                  fontSize: 20.0,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 150.0,
-                child: Divider(
-                  color: Colors.teal.shade100,
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    "+44 179 323 224",
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: "SourceSans",
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.mail,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    "jimjong@gmail.com",
-                    style: TextStyle(
-                      fontFamily: "SourceSans",
-                      fontSize: 20.0,
-                      color: Colors.teal.shade900,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+import 'homepage.dart';
+void main(){
+  runApp(MaterialApp(
+    home: navbar(),
+  ),
+  );
 }
